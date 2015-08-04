abfad532d143fccccb69d8bf4d87e70fd3f59557
Prosares <prosares@Prosaress-Mac.local>
2015-08-04 11:55:44 +0530

    Initial Commit

---
+++ colorDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  colorDemo
+//
+//  Created by Prosares on 8/4/15.
+//  Copyright (c) 2015 Prosares. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ colorDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  colorDemo
+//
+//  Created by Prosares on 8/4/15.
+//  Copyright (c) 2015 Prosares. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
